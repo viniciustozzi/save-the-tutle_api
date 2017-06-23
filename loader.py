5535685f57ee674bde7aae23163c001defc6f082
Vinicius Tozzi <tozzivinicius@gmail.com>
2017-06-23 00:16:05 -0300

    Adicionado o str() para nao dar pau no json

--- loader.py
+++ loader.py
@@ -70,14 +70,14 @@ def parse_request():
     img = Image.open(BytesIO(request.files["file"].read()))
     data = map(map_func, array(img.getdata(), np.uint8))
     var = sess.run(tf.argmax(output, 1), {x: [data]})
-    return jsonify(var[0])
+    return jsonify(str(var[0]))
 
 
 @app.route('/json', methods=['POST'])
 def parse_json_request():
     data = list(map(map_conv, request.get_json()))
     var = sess.run(tf.argmax(output, 1), {x: [data]})
-    return jsonify(var[0])
+    return jsonify(str(var[0]))
 
 
 app.run()
